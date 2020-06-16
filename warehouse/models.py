8265826cc54cc9b8c9d06ab648f2895e0f17a712
mikcheal101 <hirekaanmicheal@gmail.com>
2020-06-16 07:57:48 +0100

    hemen/TodoList model created

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoList(models.Model):
+    class Meta:
+        verbose_name = 'todo list'
+        verbose_name_plural = 'todo lists'
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=200, unique=True)
+    location = models.CharField(max_length=200, unique=True)
+    
