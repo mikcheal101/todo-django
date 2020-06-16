cdb6a4f5c7463742721d06e3e04203d384cbce9b
mikcheal101 <hirekaanmicheal@gmail.com>
2020-06-16 08:17:22 +0100

    hemen/warehouse model tests module created

---
+++ warehouse/tests/test_models.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_can_create_todo_list(self):
+        pass
+
+    def test_can_create_todo_list_item(self):
+        pass
+
+    def test_can_update_todo_list(self):
+        pass
+
+    def test_can_update_todo_list_item(self):
+        pass
+
+    def test_can_delete_todo_list(self):
+        pass
+
+    def test_can_delete_todo_list_item(self):
+        pass
+
+    def test_can_view_todo_list(self):
+        pass
+
+    def test_can_view_todo_list_item(self):
+        pass
+
+    def test_can_view_all_todo_list(self):
+        pass
+
+    def test_can_view_all_todo_list_item(self):
+        pass
