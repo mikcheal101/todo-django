cdfc6e93061e39ce3785073596820e2e1912ba74
mikcheal101 <hirekaanmicheal@gmail.com>
2020-06-16 12:02:09 +0100

    hemen/todo_list_item_views created

---
+++ warehouse/views/todo_list_item_views.py
@@ -0,0 +1,21 @@
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def get_todo_list_item(request):
+    pass
+
+
+@api_view(['POST'])
+def update_todo_list_item(request):
+    pass
+
+
+@api_view(['POST'])
+def delete_todo_list_item(request):
+    pass
+
+
+@api_view(['GET'])
+def get_all_todo_list_items(request):
+    pass
