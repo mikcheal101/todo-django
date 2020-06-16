437cc0ca206ab18790fb63716e47126b4e8ebd12
mikcheal101 <hirekaanmicheal@gmail.com>
2020-06-16 12:59:01 +0100

    hemen/todo app urls file created

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rest/v1/api/', 'rest_framework.urls'),
+    path('rest/v1/api/', include('rest_framework.urls')),
 ]
