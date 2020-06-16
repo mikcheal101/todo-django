b1b7b0b0f526e8c8bc2384efb68f821593985a8f
mikcheal101 <hirekaanmicheal@gmail.com>
2020-06-16 08:48:12 +0100

    hemen/models test completed

--- warehouse/tests/test_models.py
+++ warehouse/tests/test_models.py
@@ -1,37 +1,81 @@
+import datetime
+import random
+
 from django.test import TestCase
+from faker import Faker
+
+from warehouse.models import TodoList, TodoListItem
 
 
 class TestModels(TestCase):
 
     def setUp(self):
-        pass
+        # initialize faker
+        self.faker = Faker()
 
-    def test_can_create_todo_list(self):
-        pass
+        # random due dates
+        month = random.randint(7, 11)
+        day = random.randint(1, 28)
 
-    def test_can_create_todo_list_item(self):
-        pass
+        # set max priority
+        priority = TodoListItem.HIGH
+
+        # create a sample to do list
+        todo_list = TodoList.objects.create(name=self.faker.file_name(),
+                                            location=self.faker.file_path())
+
+        # create a sample todo list item
+        TodoListItem.objects.create(
+            todo_list=todo_list, priority=priority, name=self.faker.sentence(), notes=self.faker.paragraph(), due_date=datetime.date(2020, month, day))
 
     def test_can_update_todo_list(self):
-        pass
+        name = self.faker.file_name()
+        location = self.faker.file_path()
+
+        todo_list = TodoList.objects.all().first()
+        todo_list.name = name
+        todo_list.location = location
+        todo_list.save()
+
+        self.assertEqual(todo_list.name, name)
+        self.assertEqual(todo_list.location, location)
 
     def test_can_update_todo_list_item(self):
-        pass
+        # random due dates
+        month = 8
+        day = 21
+
+        # set low priority
+        priority = TodoListItem.LOW
+
+        due_date = datetime.date(2020, month, day)
+        name = self.faker.sentence()
+        notes = self.faker.paragraph()
+
+        todo_list_item = TodoListItem.objects.all().first()
+        todo_list_item.due_date = due_date
+        todo_list_item.priority = priority
+        todo_list_item.name = name
+        todo_list_item.notes = notes
+        todo_list_item.save()
+
+        self.assertEqual(todo_list_item.due_date, due_date)
+        self.assertEqual(todo_list_item.priority, priority)
+        self.assertEqual(todo_list_item.name, name)
+        self.assertEqual(todo_list_item.notes, notes)
 
     def test_can_delete_todo_list(self):
-        pass
+        todo_lists = TodoList.objects.all()
+        before_delete_count = todo_lists.count()
+        todo_lists.first().delete()
+        after_delete_count = todo_lists.count()
+
+        self.assertGreater(before_delete_count, after_delete_count)
 
     def test_can_delete_todo_list_item(self):
-        pass
+        todo_list_items = TodoListItem.objects.all()
+        before_delete_count = todo_list_items.count()
+        todo_list_items.first().delete()
+        after_delete_count = todo_list_items.count()
 
-    def test_can_view_todo_list(self):
-        pass
-
-    def test_can_view_todo_list_item(self):
-        pass
-
-    def test_can_view_all_todo_list(self):
-        pass
-
-    def test_can_view_all_todo_list_item(self):
-        pass
+        self.assertGreater(before_delete_count, after_delete_count)
