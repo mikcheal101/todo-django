dfa0b1439591230a8bbf74e63f996f17fb7a67e8
mikcheal101 <hirekaanmicheal@gmail.com>
2020-06-16 08:09:01 +0100

    hemen/TodoListItem model created

--- warehouse/models.py
+++ warehouse/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class TodoList(models.Model):
     class Meta:
         verbose_name = 'todo list'
@@ -12,4 +13,35 @@ class TodoList(models.Model):
 
     name = models.CharField(max_length=200, unique=True)
     location = models.CharField(max_length=200, unique=True)
-    
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class TodoListItem(models.Model):
+
+    HIGH = 'HIGH'
+    MEDIUM = 'MEDIUM'
+    LOW = 'LOW'
+    NONE = 'NONE'
+    PRIORITIES = [
+        (HIGH, HIGH),
+        (MEDIUM, MEDIUM),
+        (LOW, LOW),
+        (NONE, NONE),
+    ]
+
+    class Meta:
+        verbose_name = 'todo list item'
+        verbose_name_plural = 'todo lists items'
+
+    def __str__(self):
+        return self.name
+
+    priority = models.CharField(
+        max_length=200, choices=PRIORITIES, default=HIGH)
+    name = models.CharField(max_length=200)
+    notes = models.TextField()
+
+    due_date = models.DateField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
