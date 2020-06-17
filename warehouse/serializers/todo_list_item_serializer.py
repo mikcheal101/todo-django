7c59c0314d06dfa3f43f8d7e8facd926e353505c
mikcheal101 <hirekaanmicheal@gmail.com>
2020-06-17 01:06:09 +0100

    hemen/TodoListItemSerializer [-] reformatted

--- warehouse/serializers/todo_list_item_serializer.py
+++ warehouse/serializers/todo_list_item_serializer.py
@@ -6,4 +6,5 @@ from warehouse.models import TodoListItem
 class TodoListItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoListItem
-        fields = ['todo_list', 'priority', 'name', 'notes', 'due_date', ]
+        fields = ['todo_list',
+                  'priority', 'name', 'notes', 'due_date', ]
